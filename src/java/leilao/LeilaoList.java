d04b618547a0c3d8e7d538c1c73137c39f8edcef
Fernando <tra_fernando@hotmail.com>
2017-11-29 09:58:52 -0200

    atualiza lista clientes

--- src/java/leilao/LeilaoList.java
+++ src/java/leilao/LeilaoList.java
@@ -7,8 +7,14 @@ package leilao;
 
 import com.google.gson.Gson;
 import com.sun.org.apache.bcel.internal.generic.L2D;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Consumes;
@@ -42,16 +48,39 @@ public class LeilaoList {
      *
      * @return an instance of java.lang.String
      */
-    @GET
+   @GET
     @Produces(MediaType.APPLICATION_JSON)
     public String getJson() {
         Gson g = new Gson();
         ArrayList<Produto> lista = new ArrayList<>();
-        Produto p = new Produto("PC","Intel I7 7200", 2000);
-        lista.add(p);
-        p = new Produto("PC","Intel I5 7200U", 2500);
-        lista.add(p);              
-        return  g.toJson(lista); 
+        // try {
+        //FileReader file = new FileReader("list.json");            
+        try {
+//            Reader in = new InputStreamReader(getClass().getResourceAsStream("list.json"));
+//            
+            FileReader arq = new FileReader("list.json");
+            BufferedReader lerArq = new BufferedReader(arq);
+
+            String in = lerArq.readLine(); 
+
+//            while (in != null) {               
+//                System.out.println("linha");
+//               in += lerArq.readLine();
+//            }
+            return g.toJson(in);
+        } catch (Exception e) {
+            Produto p = new Produto("PC", "Intel I7 7200", 2000);
+            lista.add(p);
+            p = new Produto("PC", "Intel I5 7200U", 2500);
+            lista.add(p);
+            return g.toJson(lista);
+        }
+
+        //} catch (FileNotFoundException ex) {
+        //  Logger.getLogger(LeilaoList.class.getName()).log(Level.SEVERE, null, ex);
+//
+//            }
+//            
     }
 
     /**
@@ -60,8 +89,21 @@ public class LeilaoList {
      * @param content representation for the resource
      */
     @PUT
-    @Consumes(MediaType.APPLICATION_XML)
-    public void putXml(String content) {
-       
+    @Consumes(MediaType.APPLICATION_JSON)
+    public boolean putXml(String content
+    ) {
+        try {
+            System.out.println(content);
+            FileWriter file = new FileWriter("list.json");
+            file.write(content);
+            file.flush();
+            file.close();
+            return true;
+        } catch (IOException ex) {
+
+            Logger.getLogger(LeilaoList.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+
     }
 }
